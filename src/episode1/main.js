5bb013e3b35fb449521271195930ae9681d37e36
osgsm <50955473+osgsm@users.noreply.github.com>
2024-01-27 17:14:30 +0900

    Improve

--- src/episode1/main.js
+++ src/episode1/main.js
@@ -5,8 +5,13 @@ const context = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-for (let i = 0; i < 100; i += 1) {
+for (let i = 0; i < 200; i += 1) {
   context.beginPath();
+  context.strokeStyle = `rgb(
+    ${Math.random() * 255},
+    ${Math.random() * 255},
+    ${Math.random() * 255}
+  )`;
   context.moveTo(Math.random() * width, Math.random() * height);
   context.lineTo(Math.random() * width, Math.random() * height);
   context.stroke();
