1ad03cb129f4a4df41d0296a994da5b55ffbb1f9
osgsm <50955473+osgsm@users.noreply.github.com>
2024-01-27 22:43:04 +0900

    Add back and forth animation

--- src/episode3/main.js
+++ src/episode3/main.js
@@ -1,13 +1,38 @@
+import colors from 'tailwindcss/colors';
 import '../style.css';
 
 const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-for (let i = 0; i < 100; i += 1) {
+// Fix blurry canvas
+const dpr = window.devicePixelRatio || 1;
+canvas.width = window.innerWidth * dpr;
+canvas.height = window.innerHeight * dpr;
+canvas.style.width = `${window.innerWidth}px`;
+canvas.style.height = `${window.innerHeight}px`;
+context.scale(dpr, dpr);
+
+const centerY = height / 2;
+const centerX = width / 2;
+const offset = 250;
+const speed = 0.05;
+let angle = 0;
+
+const render = () => {
+  const x = centerX + Math.sin(angle) * offset * 2;
+  const y = centerY + Math.sin(angle) * offset;
+  context.clearRect(0, 0, width, height);
   context.beginPath();
-  context.moveTo(Math.random() * width, Math.random() * height);
-  context.lineTo(Math.random() * width, Math.random() * height);
-  context.stroke();
-}
+  context.arc(centerX + 50, y, 10, 0, Math.PI * 2, false);
+  context.arc(x, centerY, 10, 0, Math.PI * 2, false);
+  context.fillStyle = colors.teal[700];
+  context.fill();
+
+  angle += speed;
+
+  requestAnimationFrame(render);
+};
+
+render();
