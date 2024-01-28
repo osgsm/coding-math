e55cadba07e1de5b99efefec1481a2c17dd86716
osgsm <50955473+osgsm@users.noreply.github.com>
2024-01-28 14:41:58 +0900

    Add Lissajous curve

--- src/episode4/main.js
+++ src/episode4/main.js
@@ -5,9 +5,42 @@ const context = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-for (let i = 0; i < 100; i += 1) {
+// Fix blurry canvas
+const dpr = window.devicePixelRatio || 1;
+canvas.width = window.innerWidth * dpr;
+canvas.height = window.innerHeight * dpr;
+canvas.style.width = `${window.innerWidth}px`;
+canvas.style.height = `${window.innerHeight}px`;
+context.scale(dpr, dpr);
+
+context.translate(0, 0);
+
+const centerX = width / 2;
+const centerY = height / 2;
+const xRadius = 1200;
+const yRadius = 400;
+const numObjects = 5000;
+const slice = (Math.PI * 2) / numObjects;
+let angle = 0;
+let x;
+let y;
+
+for (let i = 0; i < numObjects; i++) {
+  angle = slice * i;
+  x = centerX + Math.cos(angle) * xRadius;
+  y = centerY + Math.sin(angle + i / 198) * yRadius;
   context.beginPath();
-  context.moveTo(Math.random() * width, Math.random() * height);
-  context.lineTo(Math.random() * width, Math.random() * height);
-  context.stroke();
+  context.arc(x, y, 2, 0, Math.PI * 2, false);
+  context.fillStyle = `hsl(${i / 100 + 240}, 100%, 60%)`;
+  context.fill();
+}
+
+for (let i = 0; i < numObjects; i++) {
+  angle = slice * i;
+  x = centerX + Math.cos(angle) * xRadius + 100;
+  y = centerY + Math.sin(angle + i / 198.5) * yRadius;
+  context.beginPath();
+  context.arc(x, y, 2, 0, Math.PI * 2, false);
+  context.fillStyle = `hsl(${i / 100 + 10}, 100%, 60%)`;
+  context.fill();
 }
