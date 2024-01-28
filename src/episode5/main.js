2f8cbaac547407553d554b08b2be829ee4ed3411
osgsm <50955473+osgsm@users.noreply.github.com>
2024-01-28 19:12:51 +0900

    Add rotating rocket

--- src/episode5/main.js
+++ src/episode5/main.js
@@ -5,9 +5,34 @@ const context = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-for (let i = 0; i < 100; i += 1) {
-  context.beginPath();
-  context.moveTo(Math.random() * width, Math.random() * height);
-  context.lineTo(Math.random() * width, Math.random() * height);
-  context.stroke();
-}
+let arrowX = width / 2;
+let arrowY = height / 2;
+let dx;
+let dy;
+let angle = 0;
+let a = 0;
+
+const render = () => {
+  arrowX = width / 2 + Math.cos(a) * width * 0.3;
+  arrowY = height / 2 + Math.sin(a) * height * 0.3;
+  a += 0.008;
+  context.clearRect(0, 0, width, height);
+
+  context.save();
+  context.translate(arrowX, arrowY);
+  context.rotate(angle);
+
+  context.font = '80px Arial';
+  context.fillText('🚀', 50, 50);
+
+  context.restore();
+  requestAnimationFrame(render);
+};
+
+render();
+
+window.addEventListener('mousemove', (event) => {
+  dx = event.clientX - arrowX;
+  dy = event.clientY - arrowY;
+  angle = Math.atan2(dy, dx);
+});
